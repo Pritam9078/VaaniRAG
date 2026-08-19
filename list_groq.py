4da113ffd8b615867c47b392cd71ef593709374a
Pritam9078 <dpritam2708@gmail.com>
2026-08-19 17:11:39 +0530

    chore: cleanup temporary scripts, tests, and backup files

--- list_groq.py
+++
@@ -1,9 +0,0 @@
-import os
-import time
-from groq import Groq
-import httpx
-
-client = Groq(api_key=os.environ.get("GROQ_API_KEY"), http_client=httpx.Client())
-models = client.models.list()
-for m in models.data:
-    print(m.id)
--- test.py
+++
@@ -1,4 +0,0 @@
-import sys
-import pickle
-from backend.rag.retrieval.retrieval import FastBM25
-sys.modules['__main__'].FastBM25 = FastBM25
